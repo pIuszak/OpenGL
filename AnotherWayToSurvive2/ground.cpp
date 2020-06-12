265680a8fdc14a82e59f8f8f616564beab39f9a5
pluszak <luszukpawel@gmail.com>
2020-06-12 18:52:07 +0200

    [add] zombie

--- AnotherWayToSurvive2/ground.cpp
+++ AnotherWayToSurvive2/ground.cpp
@@ -17,6 +17,9 @@ enum {
     SCENE,
     ARMS,
     TREE,
+    TORSO,
+    LEG_R,
+    LEG_L,
     NUMBER_OF_BUFFERS
 };
 
@@ -96,6 +99,73 @@ void DrawShadowMap()
     glUseProgram(0);
 
 }
+
+// -----------------------------------------
+class CBone
+{
+
+public:
+
+
+    // potok opengl
+    GLuint VAO;
+    int VBO_Size;
+
+    // macierz
+    glm::mat4x4 matModel;
+
+    // nast�pny obiekt typu CBone w strukturze
+    CBone *next = NULL;
+
+    // ustawienie potoku
+    void Set(GLuint _vao, int _size)
+    {
+        matModel = glm::mat4(1.0);
+        VAO = _vao;
+        VBO_Size = _size;
+    }
+
+    // rysowanie na scenie
+    void Draw(GLuint _prog, glm::mat4 _stackModel = glm::mat4(1.0))
+    {
+
+        glm::mat4 localModel = _stackModel * matModel;
+
+        glUniformMatrix4fv( glGetUniformLocation( _prog, "matModel" ), 1, GL_FALSE, glm::value_ptr(localModel) );
+
+        glBindVertexArray( VAO );
+        glDrawArrays( GL_TRIANGLES, 0, VBO_Size );
+        glBindVertexArray( 0 );
+
+        if(next)
+        {
+            next->Draw(_prog, localModel);
+        }
+
+    }
+
+    void Obroc(float angle, float x, float y, float z)
+    {
+        this->matModel = glm::rotate(matModel, angle, glm::vec3(x, y, z));
+    }
+
+    void Przesun(float x, float y, float z)
+    {
+
+        this->matModel = glm::translate(matModel, glm::vec3(x, y, z));
+    }
+
+
+};
+
+
+
+CBone Torso;
+CBone RightArm;
+CBone RightLeg;
+CBone LeftArm;
+CBone LeftLeg;
+
 
 // ----------------------------------------------------------------
 class CSceneObject {
@@ -481,7 +551,7 @@ void DisplayScene() {
     for (int i = 0; i < 100; ++i) {
         Trees[i].Draw();
     }
-
+    Torso.Draw(program[SCENE]);
 
     // WYLACZAMY program
     glUseProgram(0);
@@ -496,7 +566,10 @@ void Reshape(int width, int height) {
     glViewport(0, 0, width, height);
     matProj = glm::perspectiveFov(glm::radians(60.0f), (float) width, (float) height, 0.1f, 100.f);
 }
-
+float leftLeg1 = 0;
+float leftLeg2 = 0;
+float rightLeg1;
+float rightLeg2;
 // --------------------------------------------------------------
 void Keyboard(unsigned char key, int x, int y) {
 
@@ -517,6 +590,49 @@ void Keyboard(unsigned char key, int x, int y) {
             ypos -= float(sin(xrotrad)) / 8.0f;
             // printf("XD %f",_scene_rotate_x);
             break;
+
+
+
+        case 'x':
+
+            if(leftLeg1 > -0.5){
+                LeftLeg.Obroc(-0.1, 1.0, 0.0, 0.0);
+                RightLeg.Obroc(0.2, 1.0, 0.0, 0.0);
+                leftLeg1-= 0.1;
+                printf("1");
+                break;
+            }
+
+            if(rightLeg1 > -0.5){
+                LeftLeg.Obroc(0.1, 1.0, 0.0, 0.0);
+                RightLeg.Obroc(-0.2, 1.0, 0.0, 0.0);
+                rightLeg1-= 0.1;
+                printf("2");
+                break;
+            }
+
+            if(leftLeg2 <= 0.5){
+                LeftLeg.Obroc(0.1, 1.0, 0.0, 0.0);
+                RightLeg.Obroc(-0.2, 1.0, 0.0, 0.0);
+                leftLeg2+= 0.1;
+                printf("3");
+                break;
+            }
+            if(rightLeg2 <= 0.5){
+                LeftLeg.Obroc(-0.1, 1.0, 0.0, 0.0);
+                RightLeg.Obroc(0.2, 1.0, 0.0, 0.0);
+                rightLeg2+= 0.1;
+                printf("4");
+                break;
+            }
+
+            leftLeg2= 0;
+            leftLeg1= 0;
+            rightLeg2= 0;
+            rightLeg1= 0;
+
+            break;
+
 
     }
 
@@ -703,6 +819,106 @@ void Initialize() {
 
 
     }
+
+
+    if (!loadOBJ("torso.obj", OBJ_vertices[TORSO], OBJ_uvs[TORSO], OBJ_normals[TORSO]))
+    {
+        printf("Not loaded!\n");
+        exit(1);
+    }
+    // Vertex arrays
+    glGenVertexArrays( 1, &vArray[TORSO] );
+    glBindVertexArray( vArray[TORSO] );
+
+    glGenBuffers( 1, &vBuffer_pos[TORSO] );
+    glBindBuffer( GL_ARRAY_BUFFER, vBuffer_pos[TORSO] );
+    glBufferData(GL_ARRAY_BUFFER, OBJ_vertices[TORSO].size() * sizeof(glm::vec3), &(OBJ_vertices[TORSO])[0], GL_STATIC_DRAW );
+    glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 0, NULL );
+    glEnableVertexAttribArray( 0 );
+
+    glGenBuffers( 1, &vBuffer_uv[TORSO] );
+    glBindBuffer( GL_ARRAY_BUFFER, vBuffer_uv[TORSO] );
+    glBufferData(GL_ARRAY_BUFFER, OBJ_uvs[TORSO].size() * sizeof(glm::vec2), &(OBJ_uvs[TORSO])[0], GL_STATIC_DRAW );
+    glVertexAttribPointer( 1, 2, GL_FLOAT, GL_FALSE, 0, NULL );
+    glEnableVertexAttribArray( 1 );
+    glBindVertexArray( 0 );
+
+
+
+    if (!loadOBJ("leftleg.obj", OBJ_vertices[LEG_L], OBJ_uvs[LEG_L], OBJ_normals[LEG_L]))
+    {
+        printf("Not loaded!\n");
+        exit(1);
+    }
+    // Vertex arrays
+    glGenVertexArrays( 1, &vArray[LEG_L] );
+    glBindVertexArray( vArray[LEG_L] );
+
+    glGenBuffers( 1, &vBuffer_pos[LEG_L] );
+    glBindBuffer( GL_ARRAY_BUFFER, vBuffer_pos[LEG_L] );
+    glBufferData( GL_ARRAY_BUFFER, OBJ_vertices[LEG_L].size() * sizeof(glm::vec3), &(OBJ_vertices[LEG_L])[0], GL_STATIC_DRAW );
+    glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 0, NULL );
+    glEnableVertexAttribArray( 0 );
+
+    glGenBuffers( 1, &vBuffer_uv[LEG_L] );
+    glBindBuffer( GL_ARRAY_BUFFER, vBuffer_uv[LEG_L] );
+    glBufferData( GL_ARRAY_BUFFER, OBJ_uvs[LEG_L].size() * sizeof(glm::vec2), &(OBJ_uvs[LEG_L])[0], GL_STATIC_DRAW );
+    glVertexAttribPointer( 1, 2, GL_FLOAT, GL_FALSE, 0, NULL );
+    glEnableVertexAttribArray( 1 );
+    glBindVertexArray( 0 );
+
+
+    if (!loadOBJ("rightleg.obj", OBJ_vertices[LEG_R], OBJ_uvs[LEG_R], OBJ_normals[LEG_R]))
+    {
+        printf("Not loaded!\n");
+        exit(1);
+    }
+    // Vertex arrays
+    glGenVertexArrays( 1, &vArray[LEG_R] );
+    glBindVertexArray( vArray[LEG_R] );
+
+    glGenBuffers( 1, &vBuffer_pos[LEG_R] );
+    glBindBuffer( GL_ARRAY_BUFFER, vBuffer_pos[LEG_R] );
+    glBufferData( GL_ARRAY_BUFFER, OBJ_vertices[LEG_R].size() * sizeof(glm::vec3), &(OBJ_vertices[LEG_R])[0], GL_STATIC_DRAW );
+    glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 0, NULL );
+    glEnableVertexAttribArray( 0 );
+
+    glGenBuffers( 1, &vBuffer_uv[LEG_R] );
+    glBindBuffer( GL_ARRAY_BUFFER, vBuffer_uv[LEG_R] );
+    glBufferData( GL_ARRAY_BUFFER, OBJ_uvs[LEG_R].size() * sizeof(glm::vec2), &(OBJ_uvs[LEG_R])[0], GL_STATIC_DRAW );
+    glVertexAttribPointer( 1, 2, GL_FLOAT, GL_FALSE, 0, NULL );
+    glEnableVertexAttribArray( 1 );
+    glBindVertexArray( 0 );
+
+
+    // 1. Stworzenie kosci
+    Torso.Set(vArray[TORSO], OBJ_vertices[TORSO].size() );
+    LeftLeg.Set(vArray[LEG_L], OBJ_vertices[LEG_L].size() );
+    RightLeg.Set(vArray[LEG_R], OBJ_vertices[LEG_R].size() );
+
+    // 2. Ustalenie hierarchii
+
+    //TODO change this hierarchy somehow
+
+    Torso.next = &RightArm;
+    RightArm.next = &LeftArm;
+
+    LeftArm.next = &LeftLeg;
+    LeftLeg.next = &RightLeg;
+
+
+
+
+
+    // 3. Ustawlenie rozmieszczenia obiektow wzgledem bazowego
+    Torso.matModel = glm::translate(glm::mat4(1.0), glm::vec3(0.0, 5, 0.0));
+    RightArm.matModel = glm::translate(glm::mat4(1.0), glm::vec3(-0.3, -0.28, -0.1));
+    LeftArm.matModel = glm::translate(glm::mat4(1.0), glm::vec3(0.6, 0.0, 0.0));
+    LeftLeg.matModel = glm::translate(glm::mat4(1.0), glm::vec3(-0.3, -0.5, 0.2));
+    RightLeg.matModel = glm::translate(glm::mat4(1.0), glm::vec3(0.0, 0.0, 0.0));
+
+    //RightArm.Obroc(-0.5, 0.0, 1.0, 0.0);
+    //PropellerSecondary.Obroc(0.6, 0.0, 0.0, 1.0);
 
     // ---------- SHADOW MAPPING------------
 
